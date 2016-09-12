2a41c78d67ad7d9a2752849c094f7a97f841a3e9
rexler <rexler@users.noreply.github.com>
2016-09-12 10:13:55 +0100

    Added simple score display and stylesheet

--- src/app.js
+++ src/app.js
@@ -11,10 +11,23 @@
         var dice = engine.playRound([], document.getElementById('mainCanvas'));
         console.log('------->', dice);
 
+
         var scores = engine.getScores();
+        var div = document.getElementsByClassName('scores')[0];
+        div.innerHTML = '';
+        var table = document.createElement('table');
         engine.hands.forEach(function(hand, index) {
             console.log(hand + ": " + scores[index]);
+            var row = document.createElement('tr');
+            var td1 = document.createElement('td');
+            td1.innerText = hand;
+            var td2 = document.createElement('td');
+            td2.innerText = scores[index];
+            row.appendChild(td1);
+            row.appendChild(td2);
+            table.appendChild(row);
         });
+        div.appendChild(table);
     };
 
 })();
