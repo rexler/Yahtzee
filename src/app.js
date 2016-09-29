fc8074017f50e0bdb93351b682880dd8d3a78f89
rexler <rexler@users.noreply.github.com>
2016-09-29 12:33:41 +0100

    Refactored draw scores method into assets module

--- src/app.js
+++ src/app.js
@@ -24,36 +24,28 @@
         var dice = engine.playRound([], document.getElementById('mainCanvas'));
         console.log('------->', dice);
 
-        var scores = engine.getScores();
+        var scores = [];
+        var generatedScores = engine.getScores();
         var players = engine.getPlayers();
-        var div = document.getElementsByClassName('scores')[0];
-        div.innerHTML = '';
-        var table = document.createElement('table');
-        engine.hands.forEach(function(hand, index) {
-            //console.log(hand + ": " + scores[index]);
-            var row = document.createElement('tr');
-            var td1 = document.createElement('td');
-            td1.innerText = hand;
-            row.appendChild(td1);
-            players.forEach(function(player) {
-                var td2 = document.createElement('td');
-                if (player.scores[index] === -1) {
-                    var anchor = document.createElement('a');
-                    anchor.setAttribute('href', '#');
-                    anchor.innerText = player.scores[index] === -1 ? scores[index] : player.scores[index];
-                    anchor.onclick = submitScoreEvent;
-                    td2.appendChild(anchor);
-                }
-                else {
-                    td2.innerText = player.scores[index] === -1 ? scores[index] : player.scores[index];
-                }
-                td2.setAttribute('class', player.scores[index] === -1 ? 'green' : 'red');
-                row.appendChild(td2);
-            });
 
-            table.appendChild(row);
+        engine.hands.forEach(function(hand, index) {
+            var scoresRow = {
+                hand: hand,
+                players: []
+            };
+            players.forEach(function(player) {
+                var playerRow = {
+                    score: 0,
+                    submitted: false
+                };
+                playerRow.score = player.scores[index] === -1 ? generatedScores[index] : player.scores[index];
+                playerRow.submitted = player.scores[index] !== -1;
+                scoresRow.players.push(playerRow);
+            });
+            scores.push(scoresRow);
         });
-        div.appendChild(table);
+
+        assets.drawScores(document.getElementsByClassName('scores')[0], scores, submitScoreEvent);
     };
 
 })();
--- src/assets.js
+++ src/assets.js
@@ -8,8 +8,43 @@ var appendSVG = function(baseElement, assetName, width, height) {
     baseElement.appendChild(svg);
 };
 
+var drawScores = function(baseElement, scoreList, submitCallback) {
+    var div = baseElement;
+    div.innerHTML = '';
+    var table = document.createElement('table');
+    scoreList.forEach(function(scoreItem) {
+        var row = document.createElement('tr');
+        var td1 = document.createElement('td');
+        td1.innerText = scoreItem.hand;
+        row.appendChild(td1);
+
+        scoreItem.players.forEach(function(player) {
+            var td2 = document.createElement('td');
+            if (!player.submitted) {
+                var anchor = document.createElement('a');
+                anchor.setAttribute('href', '#');
+                // -1 means score not submitted yet
+                anchor.innerText = player.score;
+                anchor.onclick = submitCallback;
+                td2.appendChild(anchor);
+            }
+            else {
+                td2.innerText = player.score;
+            }
+            td2.setAttribute('class', player.submitted ? 'red' : 'green');
+            row.appendChild(td2);
+        });
+
+        table.appendChild(row);
+
+    });
+
+    div.appendChild(table);
+};
+
 var assets = {
-    appendSVG: appendSVG
+    appendSVG: appendSVG,
+    drawScores: drawScores
 };
 
 module.exports = assets;
